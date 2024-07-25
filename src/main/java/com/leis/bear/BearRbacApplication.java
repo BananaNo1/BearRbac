0f96337bc289c80d23b0d1b478e31858972c49c1
leis <893052525@qq.com>
2024-07-25 15:15:21 +0800

    init

---
+++ src/main/java/com/leis/bear/BearRbacApplication.java
@@ -0,0 +1,13 @@
+package com.leis.bear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearRbacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearRbacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leis/bear/BearRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leis.bear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearRbacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
