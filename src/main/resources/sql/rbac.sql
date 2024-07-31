d9ccb1b89e9eefaea9ab2892c443e55b916e54df
leis <893052525@qq.com>
2024-07-31 17:03:14 +0800

    sql

--- src/main/resources/sql/rbac.sql
+++ src/main/resources/sql/rbac.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80037
  File Encoding         : 65001
 
- Date: 25/07/2024 15:16:56
+ Date: 31/07/2024 17:02:35
 */
 
 SET NAMES utf8mb4;
@@ -30,13 +30,22 @@ CREATE TABLE `t_menu`  (
   `perms` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT 'NULL' COMMENT '权限标识',
   `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT 'NULL' COMMENT '图标',
   `type` tinyint NOT NULL COMMENT '类型 0菜单 1按钮',
-  `order_num` int NOT NULL COMMENT '排序',
+  `order_num` int NULL DEFAULT NULL COMMENT '排序',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `create_user` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '创建人',
   `update_time` datetime NOT NULL COMMENT '更新时间',
   `update_user` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '更新人',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '权限表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '权限表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_menu
+-- ----------------------------
+INSERT INTO `t_menu` VALUES (1, '系统管理', 0, '/system', 'Layout', NULL, '', 0, 1, '2024-07-25 16:39:07', 'admin', '2024-07-25 16:39:07', 'admin');
+INSERT INTO `t_menu` VALUES (2, '用户管理', 1, '/system/user', 'Layout', 'user:view', '', 0, 1, '2024-07-25 16:47:13', 'admin', '2024-07-25 06:45:55', 'admin');
+INSERT INTO `t_menu` VALUES (3, '新增用户', 2, '', '', 'user:add', NULL, 1, NULL, '2024-07-25 16:39:07', 'admin', '2024-07-25 16:39:07', 'admin');
+INSERT INTO `t_menu` VALUES (4, '修改用户', 2, '', '', 'user:update', NULL, 1, NULL, '2024-07-25 16:39:07', 'admin', '2024-07-25 16:39:07', 'admin');
+INSERT INTO `t_menu` VALUES (5, '删除用户', 2, '', '', 'user:delete', NULL, 1, NULL, '2024-07-25 16:39:07', 'admin', '2024-07-25 16:39:07', 'admin');
 
 -- ----------------------------
 -- Table structure for t_role
@@ -51,7 +60,12 @@ CREATE TABLE `t_role`  (
   `update_user` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '更新人',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`role_id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '角色表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_role
+-- ----------------------------
+INSERT INTO `t_role` VALUES (1, '管理员', '2024-07-08 16:23:11', '管理员', '2024-07-09 14:38:59', '管理员', '管理员');
 
 -- ----------------------------
 -- Table structure for t_role_menu
@@ -61,6 +75,15 @@ CREATE TABLE `t_role_menu`  (
   `menu_id` int NOT NULL COMMENT '权限ID',
   `role_id` int NOT NULL COMMENT '角色ID'
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_role_menu
+-- ----------------------------
+INSERT INTO `t_role_menu` VALUES (1, 1);
+INSERT INTO `t_role_menu` VALUES (1, 2);
+INSERT INTO `t_role_menu` VALUES (1, 3);
+INSERT INTO `t_role_menu` VALUES (1, 4);
+INSERT INTO `t_role_menu` VALUES (1, 5);
 
 -- ----------------------------
 -- Table structure for t_user
@@ -84,7 +107,12 @@ CREATE TABLE `t_user`  (
   `update_user` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '更新人',
   `remark` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES (1, 'admin', NULL, '', '49ba59abbe56e057', '', '', 0, NULL, '00', 0, '2024-07-26 10:17:02', 'admin', '2024-07-26 10:17:02', 'admin', NULL);
 
 -- ----------------------------
 -- Table structure for t_user_role
@@ -94,5 +122,10 @@ CREATE TABLE `t_user_role`  (
   `user_id` int NOT NULL COMMENT '用户ID',
   `role_id` int NOT NULL COMMENT '角色ID'
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user_role
+-- ----------------------------
+INSERT INTO `t_user_role` VALUES (1, 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
